d0595ccff6e531292dcac77180a60171a17e37d9
Roberto <hooverlds93@gmail.com>
2022-12-13 14:40:13 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, createRef } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  Container,
+  ListGroup,
+  Button,
+} from 'react-bootstrap';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+import  { v4 as uuidv4} from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function TodoList() {
+  const [items, setItems] = useState(() => [
+    {
+      id: uuidv4(),
+      text: 'Buy eggs',
+      nodeRef: createRef(null),
+    },
+    {
+      id: uuidv4(),
+      text: 'Pay bills',
+      nodeRef: createRef(null),
+    },
+    {
+      id: uuidv4(),
+      text: 'Invite friends over',
+      nodeRef: createRef(null),
+    },
+    {
+      id: uuidv4(),
+      text: 'Fix the TV',
+      nodeRef: createRef(null),
+    },
+  ]);
+  return (
+    <Container style={{ marginTop: '2rem' }}>
+      <ListGroup style={{ marginBottom: '1rem' }}>
+        <TransitionGroup className="todo-list">
+          {items.map(({ id, text, nodeRef }) => (
+            <CSSTransition
+              key={id}
+              nodeRef={nodeRef}
+              timeout={500}
+              classNames="item"
+            >
+              <ListGroup.Item ref={nodeRef}>
+                <Button
+                  className="remove-btn"
+                  variant="danger"
+                  size="sm"
+                  onClick={() =>
+                    setItems((items) =>
+                      items.filter((item) => item.id !== id)
+                    )
+                  }
+                >
+                  &times;
+                </Button>
+                {text}
+              </ListGroup.Item>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+      </ListGroup>
+      <Button
+        onClick={() => {
+          const text = prompt('Enter some text');
+          if (text) {
+            setItems((items) => [
+              ...items,
+              {
+                id: uuidv4(),
+                text,
+                nodeRef: createRef(null),
+              },
+            ]);
+          }
+        }}
+      >
+        Add Item
+      </Button>
+    </Container>
+  );
+}
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<TodoList />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
